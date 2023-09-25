faf08470765829ede4f6a411ed35e5de24089c8f
SpMex <113365369+yungmexx@users.noreply.github.com>
2023-09-24 17:15:12 -0700

    Add files via upload

---
+++ client/client.lua
@@ -0,0 +1,79 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local Result = nil
+local NUI_status = false
+
+local UseInteractSound = false
+local debug = true
+
+RegisterNetEvent('kwk-lockpick:client:openLockpick', function(callback, circles)
+    lockpickCallback = callback
+    exports['qb-lock']:StartLockPickCircle(total,circles) 
+end)
+
+function StartLockPickCircle(circles, seconds, callback)
+    Result = nil
+    if debug then
+        print(circles, "This be the lock")
+    end
+    NUI_status = true
+    SendNUIMessage({
+        action = 'start',
+        value = circles,
+		time = seconds,
+    })
+    while NUI_status do
+        Wait(5)
+        SetNuiFocus(NUI_status, false)
+    end
+    Wait(100)
+    SetNuiFocus(false, false)
+    lockpickCallback = callback
+    return Result
+end
+
+RegisterNUICallback('fail', function()
+    ClearPedTasks(PlayerPedId())
+    Result = false
+    Wait(100)
+    NUI_status = false
+    if debug then
+        print('fail')
+     end
+     if UseInteractSound then
+        TriggerServerEvent("InteractSound_SV:PlayOnSource", 'lockpickfail', 1.0)
+    else
+        PlaySoundFrontend(-1, "CANCEL", "HUD_MINI_GAME_SOUNDSET", 1 )
+    end
+end)
+
+RegisterNUICallback('success', function()
+	Result = true
+	Wait(100)
+	NUI_status = false
+    SetNuiFocus(false, false)
+    if debug then
+        print(Result)
+     end
+    if UseInteractSound then
+        TriggerServerEvent("InteractSound_SV:PlayOnSource", 'lockpicksuccess', 1.5)
+    else
+        PlaySoundFrontend(-1, "select", "HUD_MINI_GAME_SOUNDSET", 1 )
+    end
+    return Result
+end)
+
+
+
+if debug then
+    RegisterCommand("lpgame", function()
+	    local time = math.random(7,10)
+	    local circles =  math.random(2,4)
+	    local success = exports['qb-lock']:StartLockPickCircle(circles, time, success)
+	    print(success)
+	    if success then
+		    print("WIN")
+	    else
+		    print("FAIL")
+	    end
+    end)
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'SpMex'
+description 'Lockpick Circle Mini-Game'
+version '1.0'
+
+client_scripts {
+    'client/*.lua',
+}
+
+ui_page {
+    'html/index.html',
+}
+
+files {
+    'html/index.html',
+    'html/*.css',
+    'html/*.js',
+}
+
+
+exports {
+    "StartLockPickCircle"
+}
